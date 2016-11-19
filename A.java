6a413f3d6762480d5e80a477d805740873bc0c0e
Satyaprakashlitu <sswain092@gmail.com>
2016-11-20 01:24:28 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,19 @@
+
+public interface A {
+	void sum(int a,int b);
+}
+abstract class B{
+	abstract void sum(int a,int b);
+}
+public class C extends B implements AABB{
+
+	@Override
+	public void sum(int a, int b) {
+		System.out.println("hii");
+	}
+	public static void main(String[] args) {
+		C c=new C();
+		c.sum(1, 2);
+	}
+	
+}
